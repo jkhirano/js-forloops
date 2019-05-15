e457dd25d1d34d2a013c902e2dc0388785f91a90
Janelle K Hirano <jaahnelle@gmail.com>
2019-05-14 21:10:38 -1000

    added forloop Dead Presidents

--- exercises.js
+++ exercises.js
@@ -48,6 +48,21 @@ This function will iterate through the person parameter and console.log the foll
 "President person was a great leader."
 */
 
+var presidents = ['Washington', 'Adams', 'Jefferson', 'Madison', 'Monroe'];
+
+for (var i = 0; i<presidents.length; i++){
+   console.log(presidents[i]);
+}
+
+console.log(presidents.length);
+
+function leaders(person){
+   for (var i = 0; i<person.length; i++){
+     console.log('President ' + person[i] + ' was a great leader.');
+   }
+ }
+ 
+ leaders(presidents);
 
 /* 4) Line Number
 Declare a variable named `stringOfNumbers` and assign its value to an empty string. 
